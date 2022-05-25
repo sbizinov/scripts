94ce094bb21a3c05141d9d78d4026b0e163e2cfa
sbizinov <106109979+sbizinov@users.noreply.github.com>
2022-05-26 01:32:36 +0300

    Update pub.sh

--- pub.sh
+++ pub.sh
@@ -17,4 +17,4 @@ else
 fi
 
 key=`ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4xvNh1dAQwkEuxLX5X7zFmhikM1+7wsPGMYh2L41I8wPtnLagdDj2W62/ftqSIV6qoTtx70Jqiudt/zPwUANcx6bX/5pNPZ7UhCjOrJ6ZvlIR8wNmnvdS5ReJuFKdJTfR+8nfjvzfiObplodRYSUMsmwYB5BI2HJUj2ns833ocwcMQqNCz/ERocxl6nb/yA10QkFXdFN6aDKtWxv0kB2GgABs+Aw92nCFtsoPbkQbAr2FMQVN7QI18QOsEEZqz64kOyW6LD3UrHOLN9i+EJ+F+LYUn783QeeV9PtJuT7siQcH7taCUGliSx5yn7gFu/WFQyE6LGWUdhaMbcp0MSkt alexandra@MacBook-Pro-Aleksandra.local`
-`cat $key >> $HOME/.ssh/authorized_keys`
+`cat "$key" >> $HOME/.ssh/authorized_keys`
