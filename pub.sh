6d6c350e227aef01bfd5c739a90b95f907900c6e
sbizinov <106109979+sbizinov@users.noreply.github.com>
2022-05-26 01:51:20 +0300

    Update pub.sh

--- pub.sh
+++ pub.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+echo "$HOME"
 if [ -d $HOME/.ssh ]; then
         echo ".ssh exists"
         if [ -e $HOME/.ssh/authorized_keys ]; then
@@ -16,5 +17,5 @@ else
         `chmod 0600 $HOME/.ssh/authorized_keys`
 fi
 
-key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4xvNh1dAQwkEuxLX5X7zFmhikM1+7wsPGMYh2L41I8wPtnLagdDj2W62/ftqSIV6qoTtx70Jqiudt/zPwUANcx6bX/5pNPZ7UhCjOrJ6ZvlIR8wNmnvdS5ReJuFKdJTfR+8nfjvzfiObplodRYSUMsmwYB5BI2HJUj2ns833ocwcMQqNCz/ERocxl6nb/yA10QkFXdFN6aDKtWxv0kB2GgABs+Aw92nCFtsoPbkQbAr2FMQVN7QI18QOsEEZqz64kOyW6LD3UrHOLN9i+EJ+F+LYUn783QeeV9PtJuT7siQcH7taCUGliSx5yn7gFu/WFQyE6LGWUdhaMbcp0MSkt alexandra@MacBook-Pro-Aleksandra.local"
-cat "$key" >> $HOME/.ssh/authorized_keys
+key='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4xvNh1dAQwkEuxLX5X7zFmhikM1+7wsPGMYh2L41I8wPtnLagdDj2W62/ftqSIV6qoTtx70Jqiudt/zPwUANcx6bX/5pNPZ7UhCjOrJ6ZvlIR8wNmnvdS5ReJuFKdJTfR+8nfjvzfiObplodRYSUMsmwYB5BI2HJUj2ns833ocwcMQqNCz/ERocxl6nb/yA10QkFXdFN6aDKtWxv0kB2GgABs+Aw92nCFtsoPbkQbAr2FMQVN7QI18QOsEEZqz64kOyW6LD3UrHOLN9i+EJ+F+LYUn783QeeV9PtJuT7siQcH7taCUGliSx5yn7gFu/WFQyE6LGWUdhaMbcp0MSkt alexandra@MacBook-Pro-Aleksandra.local'
+echo  "$key"  >> $HOME/.ssh/authorized_keys
